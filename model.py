46678ebcce023f0d1bb952bb0ffb8c43aa1f5e5f
Tarak200524 <tarak1559@gmail.com>
2026-01-30 22:00:20 +0530

    Update YOLO model and training parameters

--- model.py
+++ model.py
@@ -1,18 +1,15 @@
 from ultralytics import YOLO
 import torch
-
 print("GPU:", torch.cuda.get_device_name(0))
-
-model = YOLO("yolo11s.pt")   # SMALL model
-
+model = YOLO("yolo11m.pt")  
 model.train(
     data="data.yaml",
-    epochs=40,
-    imgsz=512,
-    batch=6,
+    epochs=40,              
+    imgsz=640,
+    batch=4,            
     device=0,
-    workers=0,
-    cache="ram",
+    workers=0,               
+    cache="ram",             
 
     optimizer="AdamW",
     lr0=0.0025,
@@ -20,7 +17,8 @@ model.train(
     weight_decay=0.0003,
     momentum=0.937,
 
-    mosaic=0.25,
+
+    mosaic=0.25,             
     mixup=0.10,
     copy_paste=0.10,
     hsv_s=0.45,
@@ -28,9 +26,15 @@ model.train(
     fliplr=0.45,
     flipud=0.10,
     scale=0.55,
-    erasing=0.40,
+    erasing=0.40,            
 
-    amp=True,          # 🔥 VERY IMPORTANT
-    patience=20,
+   
+    amp=False,
+
+
+    freeze=None,
+
+   
+    patience=20,            
     pretrained=True,
 )
